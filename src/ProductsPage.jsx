27ee813f4438f461bd70213a3a6e74d3d32b77e2
Brian Cloutier <bmcloutier@gmail.com>
2024-07-31 13:50:15 -0500

    Added index component

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,7 +1,22 @@
+import { ProductsIndex } from "./ProductsIndex";
+import { useState, useEffect } from "react";
+import axios from "axios";
 export function ProductsPage() {
+  const [products, setProducts] = useState([]);
+
+  const handleIndex = () => {
+    console.log("handleIndex");
+    axios.get("http://localhost:3000/products.json").then((response) => {
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  };
+
+  useEffect(handleIndex, []);
+
   return (
     <main>
-      <h1>Welcome to React!</h1>
+      <ProductsIndex products={products} />
     </main>
   );
 }
