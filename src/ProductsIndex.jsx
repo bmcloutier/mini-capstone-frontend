e0e11c3d416322eb47245a0e59221f3e09a01b2a
Brian Cloutier <bmcloutier@gmail.com>
2024-08-16 12:24:16 -0500

    Added search input to dynamically filter products

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 export function ProductsIndex({ products, onShow }) {
+  const [searchFilter, setSearchFilter] = useState("");
   return (
     <div>
       <h1>All Products</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <img src={product.images[0].url} alt="" />
-          <p>Price: {product.price}</p>
-          <button onClick={() => onShow(product)}>More info</button>
-        </div>
-      ))}
+      Search: <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      {products
+        .filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <img src={product.images[0].url} alt="" />
+            <p>Price: {product.price}</p>
+            <button onClick={() => onShow(product)}>More info</button>
+          </div>
+        ))}
     </div>
   );
 }
